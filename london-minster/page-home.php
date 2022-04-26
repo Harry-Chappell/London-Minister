fbbe4cfb7db5a54d69c7851caec6fa83a251b7dc
Harry Chappell <harryc@waxmangroup.co.uk>
2022-04-26 12:26:06 +0100

    Today section desktop size
    and some tweaks

--- london-minster/page-home.php
+++ london-minster/page-home.php
@@ -21,7 +21,7 @@
 
 		<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-			<section class="hero wrap">
+			<section class="hero wrap cf">
 
 				<h1>A warm welcome <br>to London Minster</h1>
 				<h2>The heart of London for over 900 years</h2>
@@ -52,6 +52,67 @@
 						</div>
 					</a>
 
+				</div>
+			</section>
+
+			<section class="today wrap cf">
+				<div class="today--cntr d-2of3 t-1of2 m-all">
+					<div class="bg-white border-top d-1of2 t-all m-all">
+						<h4 class="today--date"><?php echo date("l d M Y"); ?></h4>
+						<h2 class="today--title">Today at London Minster</h2>
+						<?php
+							$today = date('Ymd');
+							$args = array(
+								'post_type'     => 'events',
+								'nopaging'      => true,
+								'orderby'       => 'meta_value_num',
+								'meta_key'      => 'date', //ACF date field
+								'meta_query'    => array( array(
+									'key' => 'date', 
+									'value' => $today, 
+									'compare' => '=', 
+									'type' => 'DATE'
+								))
+							);
+							$upcoming_events = new WP_Query( $args );
+							if ( $upcoming_events->have_posts() ) :
+						?>
+						<div class="today--events-cntr">
+							<table class="today--events">
+
+							<?php while ( $upcoming_events->have_posts() ) : $upcoming_events->the_post(); ?>
+
+								<tr>
+									<td><?php echo get_field('start_time');?> - <?php echo get_field('end_time'); ?>
+									<td><?php the_title(); ?><br>
+								</tr>
+
+							<?php endwhile; wp_reset_postdata(); ?>
+
+							</table>
+						</div>
+						<?php endif; ?>
+
+						<a href="#" class="today--link">
+							<span>View all service times</span>
+							<?php echo file_get_contents( home_url() . '/wp-content/svgs/Button-Circle-1.svg' ); ?>
+						</a>
+						<a href="#" class="today--link">
+							<span>General viewing times</span>
+							<?php echo file_get_contents( home_url() . '/wp-content/svgs/Button-Circle-1.svg' ); ?>
+						</a>
+						<a href="#" class="today--link">
+							<span>All latest events</span>
+							<?php echo file_get_contents( home_url() . '/wp-content/svgs/Button-Circle-1.svg' ); ?>
+						</a>
+					</div>
+				</div>
+
+				<div class="today--about-cntr d-1of3 t-1of2 m-all">
+					<h4>About London Minster</h4>
+					<h2>What makes the Minster special</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tellus justo, aliquam a pulvinar vitae, consequat eget nisi. Aenean feugiat in enim nec commodo. Donec volutpat enim enim, at laoreet augue varius vitae. Morbi diam leo, suscipit at placerat eget, convallis et ligula.</p>
+					<a href="#" class="btn btn-small">Find out more</a>
 				</div>
 			</section>
 
